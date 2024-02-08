eeb5eb84f7bc8de95d990818d1bb2b99fc224d91
Joel Löf <jlof@evolution.com>
2024-02-08 18:09:14 +0100

    feat: add types

---
+++ index.d.ts
@@ -0,0 +1,14 @@
+/**
+ * Run a TypeScript script using runts
+ *
+ * @param {string} script - path to script
+ * @returns {Promise<string>}
+ */
+export declare function runts(script: string): Promise<void>;
+/**
+ * Run a TypeScript script using runts
+ *
+ * @param {string} script - path to script
+ * @returns {Promise<string>}
+ */
+export default function runts(script: string): Promise<void>;
